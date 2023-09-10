3d8459a2c3213571745a323a6a118615ade74dd3
Abubakar Kabir Auta <abubakarkabirauta@gmail.com>
2023-09-10 22:44:56 +0100

    repo1

--- tasks/views.py
+++ tasks/views.py
@@ -28,9 +28,9 @@ def get_info(request):
     response_data = {
         "slack_name": slack_name,
         "current_day": current_day,
-        "utc_time": current_utc_time,
+        "current_utc_time": current_utc_time,
         "track":track,
-        "github_file_url": github_repo_url,
+        "github_repo_url": github_repo_url,
         "github_file_url":github_file_url,
         "status_code":200
     }
