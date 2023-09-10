340d579542d3ed6e9a91dd61cfbd06a049a7deeb
Abubakar Kabir Auta <abubakarkabirauta@gmail.com>
2023-09-10 14:30:56 +0100

    urr

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path('api', views.get_info , name = 'get_info'),
-    #path('api/', views.get_info , name = 'get_info'),
-
-    path('', views.home , name = 'home' ),
 ]
